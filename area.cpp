a03278d0d2ddb8c0dd4bfb2fd198566f1318ae33
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-03-17 05:03:53 -0300

    use const PI

--- area.cpp
+++ area.cpp
@@ -3,11 +3,13 @@
 int main () {
   std::cout << "Enter the radius: ";
 
+  const double PI = 3.1425;
+
   double radius;
 
   std::cin >> radius;
   
-  double area = 3.1415 * radius * radius;
+  double area = PI * radius * radius;
 
   std::cout << "The area is: " << area;
   
