77ecb1a03141a0b45d0ed65c073ddc425c94e1fa
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-04-10 08:44:51 -0300

    fix(cpp): add deallocation lines.cpp

--- advanced/memory-leak.cpp
+++ advanced/memory-leak.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 void badfunc() {
     int *p4 = new int{42};
-    // ...
+    // delete p4;
+    // *p4 = 99;
     return;
 }
 
 int main () {
     int i{1};
     int *p1 = &i;
-f
     cout << *p1 << endl;
 
     int *p2 = new int;
