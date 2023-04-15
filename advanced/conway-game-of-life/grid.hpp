011633f2a478917e8643e40fd3cf508bdfd46a2f
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-04-15 18:08:04 -0300

    chore(hpp): fix formatting

--- advanced/conway-game-of-life/grid.hpp
+++ advanced/conway-game-of-life/grid.hpp
@@ -1,23 +1,24 @@
 #ifndef GRID_H_
 #define GRID_H_
 
+#include <cstdlib>
 #include <string>
 #include <vector>
-#include <cstdlib>
 
 #include "cell.hpp"
 
 class grid {
-    cell cells[rowmax + 2][colmax + 2];
+  cell cells[rowmax + 2][colmax + 2];
+
 public:
-    void create(int row, int column);
-    void draw();
-    void randomize();
-    bool will_survive(int row, int column);
-    bool will_create(int row, int column);
-    void update(const grid& next);
+  void create(int row, int column);
+  void draw();
+  void randomize();
+  bool will_survive(int row, int column);
+  bool will_create(int row, int column);
+  void update(const grid &next);
 };
 
-void calculate(grid& old_generation, grid& new_generation);
+void calculate(grid &old_generation, grid &new_generation);
 
-#endif  // GRID_H_  
+#endif // GRID_H_
